f2cb9464f2717f6a1d4330e177275527ee839013
thiegorechi <rechithiego@gmail.com>
2026-03-23 10:01:15 -0600

    Create React app with name and course

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders my name on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Thiego Gomes Rechi/i);
+  expect(nameElement).toBeInTheDocument();
+});
+
+test('renders course name on the page', () => {
+  render(<App />);
+  const courseElement = screen.getByText(/Enterprise Computing/i);
+  expect(courseElement).toBeInTheDocument();
 });
